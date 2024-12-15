bf4d1cd5a9ec75fbda41125dae57209e360977cf
David_Yousry <davidyousry45@gmail.com>
2024-12-15 05:12:06 +0200

    Simulation final

--- src/main/java/org/example/Location.java
+++ src/main/java/org/example/Location.java
@@ -8,10 +8,10 @@ public class Location {
     public static final double MAX_LATITUDE = 30.20;
     public static final double MIN_LONGITUDE = 30.80;
     public static final double MAX_LONGITUDE = 31.30;
-    public static final double DETECTION_LATITUDE_OFFSET = 0.0000548;
-    public static final double DETECTION_LONGITUDE_OFFSET = 0.000063;
-    public static final double MAX_MOVEMENT_OFFSET = 0.0005;
-    public static final double MIN_MOVEMENT_OFFSET = 0.0001;
+    public static final double DETECTION_LATITUDE_OFFSET = 0.0548;
+    public static final double DETECTION_LONGITUDE_OFFSET = 0.063;
+    public static final double MAX_MOVEMENT_OFFSET = 0.008;
+    public static final double MIN_MOVEMENT_OFFSET = 0.003;
 
 
     public Location(double latitude, double longitude) {
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -42,12 +42,12 @@ public class Main {
         while (true) {
             //TODO make it sleep for 10 milliseconds, and make the detection percentage so low
             // so that the robots will move more frequently and the detection will be less frequent
-            Thread.sleep(5000);
+            Thread.sleep(2000);
 
 
 
             // TODO make it move for 5 seconds then save in DB
-            newRobots = moveRobotsSimultaneously(SubscriberService.robots,2);
+            newRobots = moveRobotsSimultaneously(SubscriberService.robots,5);
 
 
             // TODO make the detection a 10% chance
